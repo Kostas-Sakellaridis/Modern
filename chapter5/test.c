ff77b6a25ed4afa8d0cc44e99249c91694f187bb
Phugegwrins <kostas.sakellaridis@gmail.com>
2015-08-25 03:13:10 +0300

    addedd into chapter5 test

--- chapter5/test.c
+++ chapter5/test.c
@@ -4,8 +4,20 @@
 
 #include "stdio.h"
 
-int main(){
+int main() {
 
+    float commission, value;
+    printf("Enter value of trade: ");
+    scanf("%f", &value);
 
+    if (value < 2500.00f)
+        commission = 30.00f + .017f * value;
+    else if (value < 6250.00f)
+        commission = 76.00f + .0034f * value;
+    else if (value < 20000.00f)
+        commission = 76.00f + .0034f * value;
+    else commission = 155.00f + 0.11f * value;
+    
 
+    return 0;
 }
